4fb856fb5e8a8f6866bcd7f95d30e3d5def2ccd7
Estella Peh <estellapeh@gmail.com>
2022-06-12 14:23:14 +0800

    refactor

--- src/views/Login/Modal/components/button.js
+++ src/views/Login/Modal/components/button.js
@@ -1,18 +1,68 @@
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
+import Button from "@mui/material/Button";
+import React, { useState } from "react";
+import Radio from "@mui/material/Radio";
+import { styled } from "@mui/material/styles";
 
-const PurpleButton = styled(Button)({
-  width: 'auto',
+const NewButton = styled(Button)({
+  width: "auto",
   marginBottom: 6,
+  boxShadow: "none",
+  textTransform: "none",
+  textEmphasisColor: "#f5f5f3",
   fontSize: 16,
-  padding: '6px 12px',
-  border: '1px solid',
-  backgroundColor: '#7134eb',
-  textTransform: 'none',
-  '&:hover': {
-      backgroundColor: '#7134eb',
-      opacity: '90%'
-  }
+  padding: "6px 12px",
+  border: "1px solid",
+  lineHeight: 1.5,
+  backgroundColor: "#f5f5f3",
+  borderColor: "#f5f5f3",
+  "&:hover": {
+    backgroundColor: "#dbdbdb",
+    borderColor: "#dbdbdb",
+    boxShadow: "none",
+  },
 });
 
-export default PurpleButton;
+const RadioButton = () => {
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const handleChange = (e) => {
+    setSelectedValue(e.target.value);
+  };
+
+  return (
+    <Radio
+      color="default"
+      checked={selectedValue === "a"}
+      onChange={handleChange}
+    />
+  );
+};
+
+const AddTagButton = styled(Button)({
+  height: 30,
+  borderRadius: 20,
+  fontWeight: 700,
+  borderColor: "black",
+  color: "black",
+  textTransform: "none",
+  "&:hover": {
+    opacity: "70%",
+    borderColor: "black",
+  },
+});
+
+const PurpleButton = styled(Button)({
+  width: "auto",
+  marginBottom: 6,
+  fontSize: 16,
+  padding: "6px 12px",
+  border: "1px solid",
+  backgroundColor: "#7134eb",
+  textTransform: "none",
+  "&:hover": {
+    backgroundColor: "#7134eb",
+    opacity: "90%",
+  },
+});
+
+export { NewButton, RadioButton, PurpleButton, AddTagButton };
