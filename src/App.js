a8c24c09599e973d3e2b81ef08beeee2fb8855e4
Estella Peh <estellapeh@gmail.com>
2022-06-15 12:37:24 +0800

    added upcoming view & scroll thingy

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import store from './store';
 
 import Loader from './components/Loader';
 import Login from "./views/Login";
+import Upcoming from './views/Upcoming';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<Login />} />
+            <Route path="/upcoming" element={<Upcoming />} />
           </Routes>
         </Router>
       </ThemeProvider>
